1c262515919137c95a91cb186f41443e12ef2d95
Thanh Tran <thanhngoctruc.tran@sjsu.edu>
2020-03-20 11:23:38 -0700

    activity_main.xml done

--- app/src/main/java/edu/sjsu/android/stylist/MainActivity.java
+++ app/src/main/java/edu/sjsu/android/stylist/MainActivity.java
@@ -1,14 +1,40 @@
 package edu.sjsu.android.stylist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_bar);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_home) {
+
+            }
+//            else if (itemId == R.id.action_closet) {
+//
+//            } else if (itemId == R.id.action_model) {
+//
+//            } else if (itemId == R.id.action_runway) {
+//
+//            } else if (itemId == R.id.action_collection) {
+//
+//            }
+        return true;
     }
 }
