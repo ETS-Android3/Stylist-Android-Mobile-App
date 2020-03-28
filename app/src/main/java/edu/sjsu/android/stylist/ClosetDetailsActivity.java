0090560ebd67ee07de8b9d99e597f55454d23873
Nhu Nguyen <nhu.n.nguyen@sjsu.edu>
2020-03-27 21:47:09 -0700

    added camera feature, photo saved in Pictures

--- app/src/main/java/edu/sjsu/android/stylist/ClosetDetailsActivity.java
+++ app/src/main/java/edu/sjsu/android/stylist/ClosetDetailsActivity.java
@@ -2,12 +2,24 @@ package edu.sjsu.android.stylist;
 
 import android.app.Activity;
 import android.content.ContentProviderClient;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.TextView;
 
+import androidx.core.content.FileProvider;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 // This is to display the images for both Tops and Bottoms
 // Can change the viewTitle's text to Tops or Bottoms depends
@@ -17,6 +29,7 @@ public class ClosetDetailsActivity extends Activity {
     GridView gridClosetDetails;
     FloatingActionButton fabAddPhoto;
     TextView viewTitle;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +42,50 @@ public class ClosetDetailsActivity extends Activity {
 
         fabAddPhoto = (FloatingActionButton) findViewById(R.id.fab_add_photo);
 
-//        fabAddPhoto.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        fabAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    dispatchTakePhotoIntent();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void dispatchTakePhotoIntent() throws IOException {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null)
+        {
+            File photoFile = null;
+            photoFile = createPhotoFile();
+            if (photoFile != null)
+            {
+                String pathToFile = photoFile.getAbsolutePath();
+                Uri photoUri = FileProvider.getUriForFile(this, "com.domain.fileprovider", photoFile);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createPhotoFile() throws IOException
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String photoFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File photo = null;
+        try
+        {
+            photo = File.createTempFile(
+                    photoFileName, /* prefix */
+                    ".jpg", /* surfix */
+                    storageDir);
+        } catch (IOException e)
+        {
+            Log.d("log", "Exception: " + e.toString());
+        }
+        return photo;
     }
 }
