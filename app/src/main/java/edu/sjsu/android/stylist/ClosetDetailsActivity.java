838f7155af5fd70a162f8ce9d2450a9c7fa6af7d
Nhu Nguyen <nhu.n.nguyen@sjsu.edu>
2020-03-31 21:04:39 -0700

    created dialog to choose between camera and gallery

--- app/src/main/java/edu/sjsu/android/stylist/ClosetDetailsActivity.java
+++ app/src/main/java/edu/sjsu/android/stylist/ClosetDetailsActivity.java
@@ -1,15 +1,23 @@
 package edu.sjsu.android.stylist;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentProviderClient;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.core.content.FileProvider;
@@ -29,15 +37,24 @@ public class ClosetDetailsActivity extends Activity {
     GridView gridClosetDetails;
     FloatingActionButton fabAddPhoto;
     TextView viewTitle;
+    ImageView viewImage;
+    String pathToFile = null;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int PERMISSION_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closet_details);
 
+        if (Build.VERSION.SDK_INT >= 23)
+        {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+
         // This is just an empty gridview since I'm not sure what else I should add - Phoenix
         gridClosetDetails = (GridView) findViewById(R.id.grid_closet_details);
+        viewImage = (ImageView) findViewById(R.id.view_photo);
         viewTitle = (TextView) findViewById(R.id.title_closet_details);
 
         fabAddPhoto = (FloatingActionButton) findViewById(R.id.fab_add_photo);
@@ -45,11 +62,40 @@ public class ClosetDetailsActivity extends Activity {
         fabAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    dispatchTakePhotoIntent();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    dispatchTakePhotoIntent();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (options[which].equals("Take Photo"))
+                        {
+                            try {
+                                dispatchTakePhotoIntent();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        else if (options[which].equals("Choose from Gallery"))
+                        {       
+
+                        }
+                        else if (options[which].equals("Cancel"))
+                        {
+                            dialog.dismiss();
+                        }
+
+
+                    }
+                });
+                builder.show();
+
             }
         });
     }
@@ -60,32 +106,55 @@ public class ClosetDetailsActivity extends Activity {
         {
             File photoFile = null;
             photoFile = createPhotoFile();
+
             if (photoFile != null)
             {
-                String pathToFile = photoFile.getAbsolutePath();
-                Uri photoUri = FileProvider.getUriForFile(this, "com.domain.fileprovider", photoFile);
+                pathToFile = photoFile.getAbsolutePath();
+                Log.d("log", photoFile.getAbsolutePath());
+                Uri photoUri = FileProvider.getUriForFile(this, "edu.sjsu.android.stylist.fileprovider", photoFile);
                 takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                 startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
-
     private File createPhotoFile() throws IOException
     {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String photoFileName = "JPEG_" + timeStamp + "_";
+        String photoFileName = "JPG_stylist_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File photo = null;
-        try
-        {
-            photo = File.createTempFile(
-                    photoFileName, /* prefix */
-                    ".jpg", /* surfix */
-                    storageDir);
+        try {
+            photo =  File.createTempFile(photoFileName, ".jpg", storageDir);
+            photo = new File(storageDir, photoFileName + ".jpg");
         } catch (IOException e)
         {
-            Log.d("log", "Exception: " + e.toString());
+            Log.d("log", "Exception" + e.toString());
         }
         return photo;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap myBitmap = BitmapFactory.decodeFile(pathToFile);
+            viewImage.setImageBitmap(myBitmap);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //Granted.
+
+                }
+                else{
+                    //Denied.
+                }
+                break;
+        }
     }
 }
