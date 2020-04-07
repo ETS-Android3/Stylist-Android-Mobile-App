1b816d2d9c947da3ff111ef92b7575aad19157dc
Nhu Nguyen <nhu.n.nguyen@sjsu.edu>
2020-04-06 18:23:47 -0700

    added edittext and spinner to get info

--- app/src/main/java/edu/sjsu/android/stylist/PhotoDisplayActivity.java
+++ app/src/main/java/edu/sjsu/android/stylist/PhotoDisplayActivity.java
@@ -5,16 +5,33 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 // This activity displays the photo user has chosen from gallery or taken
 // from camera. Useful for testing. Can be removed later I think - Phoenix
-public class PhotoDisplayActivity extends Activity {
+public class PhotoDisplayActivity extends Activity implements AdapterView.OnItemSelectedListener {
     ImageView viewPhotoDisplay;
     Button buttonOK;
+    EditText editName;
+    Spinner spinnerClothingTypes;
+
+    String photoPath;
+    String clothingType;
+    String name;
+
+    List<String> clothingTypes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +47,15 @@ public class PhotoDisplayActivity extends Activity {
             }
         });
 
+        editName = (EditText) findViewById(R.id.edit_name);
+        spinnerClothingTypes = (Spinner) findViewById(R.id.spinner_clothing_types);
+        spinnerClothingTypes.setOnItemSelectedListener(this);
+
+        populateSpinner();
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, clothingTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerClothingTypes.setAdapter(adapter);
+
         getIncomingPhoto();
 
     }
@@ -38,13 +64,38 @@ public class PhotoDisplayActivity extends Activity {
     {
         if (getIntent() != null)
         {
+            photoPath = getIntent().getStringExtra("photoPath");
             Bitmap myBitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("photoPath"));
             viewPhotoDisplay.setImageBitmap(myBitmap);
         }
     }
     private void dispatchReturnIntent()
     {
+        name = editName.getText().toString();
+
+        // TODO save this to database
+        //addToDatabase(photoPath, name, clothingType);
+        Log.d("log", "command " + photoPath + " " + name + " " + clothingType);
+
         Intent returnIntent = new Intent(this, ClosetDetailsActivity.class);
         startActivity(returnIntent);
+    }
+
+    private void populateSpinner()
+    {
+        clothingTypes = new ArrayList<String>();
+        clothingTypes.add("Tops");
+        clothingTypes.add("Bottoms");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        clothingType = parent.getItemAtPosition(position).toString();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
